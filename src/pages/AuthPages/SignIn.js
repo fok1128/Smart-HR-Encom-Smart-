0f27004174daf23520cf815c2ee123ec1428b826
fok1128 <fok112869@gmail.com>
2026-01-28 09:47:25 +0700

    Fix build: prevent tsc emitting JS; use typecheck + vite build

--- src/pages/AuthPages/SignIn.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import SignInForm from "../../components/auth/SignInForm";
-import AuthPageLayout from "./AuthPageLayout";
-export default function SignIn() {
-    return (_jsx(AuthPageLayout, { children: _jsx(SignInForm, {}) }));
-}
