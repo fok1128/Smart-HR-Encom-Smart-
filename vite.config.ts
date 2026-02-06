1415a98114274e325aa7de6b8b512b3fbd3a6722
fok1128 <fok112869@gmail.com>
2026-02-06 16:36:14 +0700

    chore: adjust vite chunk warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   plugins: [react(), svgr()],
   base: "./",
+  build: {
+    // ปรับเพดาน warning (หน่วยเป็น kB) — ของคุณไฟล์หลัก ~1621kB
+    chunkSizeWarningLimit: 2000,
+
+  },
 });
