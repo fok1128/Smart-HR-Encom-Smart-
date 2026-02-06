bc6dd8b3be4efd4823c35f0eb48a5126e4ec2b48
fok1128 <fok112869@gmail.com>
2026-02-06 16:41:38 +0700

    chore: update vite config (chunk warning limit)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+
 export default defineConfig({
-    plugins: [
-        react(),
-        svgr(), // ✅ เพิ่มบรรทัดนี้
-    ],
+  plugins: [react(), svgr()],
+  base: "./",
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
 });
