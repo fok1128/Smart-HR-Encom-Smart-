a2cc880d8183a5ac77435e88a78ff9653076335b
fok1128 <fok112869@gmail.com>
2026-02-28 11:59:54 +0700

    Fix iOS attachment

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const corsOptions = {
   origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma", "Accept"],
   maxAge: 86400,
 };
 
